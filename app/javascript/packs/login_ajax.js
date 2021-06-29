5b2cd9a959b0b8141ad09cfbd644129095405498
Hieu.vuminh <hieu.vuminh@pixta.co.jp>
2021-06-29 10:00:25 +0700

    create session

--- app/javascript/packs/login_ajax.js
+++ app/javascript/packs/login_ajax.js
@@ -9,9 +9,28 @@ $('#submit-login-form').click(function () {
         },
         success: function (data) {
             $('#notice').text(data.message);
+            if (data.status == "SUCCESS") {
+                user = data.data;
+                console.log(data.data);
+                submit_to_controller(data.data)
+            }
         },
         error: function (data) {
             $('#notice').text(data.message);
         }
     })
 });
+
+function submit_to_controller (user) {
+    $.ajax({
+        url: "/session",
+        type: "post",
+        data: {
+            "user": user
+        },
+        success: function (data) {
+        },
+        error: function (data) {
+        }
+    })
+};
